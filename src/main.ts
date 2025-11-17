4900487a5a48d97e2e19f53300bc766bfe380aab
Gabor Halasz <gabor.halasz@level14.hu>
2025-11-17 18:03:36 +0100

    Added prisma and class validator support

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import path from 'node:path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(path.join(__dirname, '..', '..', 'public'));
+  app.setBaseViewsDir(path.join(__dirname, '..', '..', 'views'));
 
   app.setViewEngine('ejs');
 
